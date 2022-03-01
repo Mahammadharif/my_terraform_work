64ff4bddc09ae05676a4c5c28bbfd54137fb0492
mhshaik <harifsk09@gmail.com>
2022-03-01 20:36:36 +0530

    added outputs , display name and changed instance type

--- aws_my_ec2/main.tf
+++ aws_my_ec2/main.tf
@@ -17,13 +17,25 @@ provider "aws" {
 
 # Sample Ec2 creation in Ohio location 
 
-resource "aws_instance" "ec2_ubuntu" {
+resource "aws_instance" "my_ec2_ubuntu" {
 
 ami                             = "ami-05803413c51f242b7"
-instance_type                   = "t2.nano"
+instance_type                   = "t2.micro" #changed from nano to micro
 associate_public_ip_address     = true
 tags                            = {
-    name                        = "ec2_instance"
+    Name                        = "my_ec2_instance" # "Name will be used for display name in tags sections "
 }
 
+}
+# output instance id
+output "my_ec2_instance_id" {
+    value = aws_instance.my_ec2_ubuntu.id
+}
+#output instance public ip
+output "my_ec2_instance_publicip"  {
+    value = aws_instance.my_ec2_ubuntu.public_ip
+}
+# output instance arn 
+output "my_ec2_instance_arn" {
+    value = aws_instance.my_ec2_ubuntu.arn
 }
