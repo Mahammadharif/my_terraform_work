e56e3ae3332e0fa3c4ff16341cd3651f616e89c5
mhshaik <harifsk09@gmail.com>
2022-03-02 10:04:37 +0530

    scan on push and encryption added with AES256

--- aws_my_ecr/main.tf
+++ aws_my_ecr/main.tf
@@ -26,7 +26,12 @@ resource "aws_ecr_repository" "my_test_ecr_repo" {
 
     name                    = "my_test_repo"
     image_scanning_configuration  {
-        scan_on_push        = "false"
+        scan_on_push        = "true"
+        
+    }
+
+    encryption_configuration {
+        encryption_type     = "AES256"
     }
 
 
