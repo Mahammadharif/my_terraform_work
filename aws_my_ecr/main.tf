92c0c06d8c09d335c2476d96719e64b2b8603c84
mhshaik <harifsk09@gmail.com>
2022-03-02 09:56:31 +0530

    aws_my_ecr created

---
+++ aws_my_ecr/main.tf
@@ -0,0 +1,33 @@
+# provider decluration 
+
+terraform {
+    required_providers {
+        aws                 = {
+            source          = "registry.terraform.io/hashicorp/aws"
+            version         = "~> 3.27"
+            
+        }
+    }
+    required_version        = ">= 0.12"
+}
+
+#provider decluration     
+
+provider "aws" {
+    region                  = "us-east-2"
+}
+
+# test ecr repository creation 
+resource "aws_ecr_repository" "my_test_ecr_repo" {
+    tags                    = {
+        Name                = "my_test_ecr_repo"
+
+    }
+
+    name                    = "my_test_repo"
+    image_scanning_configuration  {
+        scan_on_push        = "false"
+    }
+
+
+}
